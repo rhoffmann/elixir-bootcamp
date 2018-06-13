1835fb1f3c26b86cbba72c08225bff405315bcf2
Richard Hoffmann <r.hoffmann@crolla-lowis.de>
2018-06-13 19:18:04 +0200

    comprehensions

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,6 +1,13 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three", "Four"]
+    values = ["Ass", "Zwei", "Drei", "Vier", "Fünf", "Sechs", "Sieben", "Acht", "Neun", "Zehn", "Bube", "Dame", "König"]
+    suits = ["Kreuz", "Pik", "Herz", "Karo"]
+
+    for suit <- suits do
+      for value <- values do
+        "#{suit} #{value}"
+      end
+    end
   end
 
   def shuffle(deck) do
