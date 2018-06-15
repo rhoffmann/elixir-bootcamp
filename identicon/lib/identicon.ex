81a0d26873a9e926e5dd0fb0ee45d4d3ec17101e
Richard Hoffmann <r.hoffmann@crolla-lowis.de>
2018-06-15 14:31:01 +0200

    color and mirror row

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,18 +1,32 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
+  def main(input) do
+    input
+    |> hash_input
+    |> get_color
+    |> create_grid
+  end
 
-  @doc """
-  Hello world.
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
 
-  ## Examples
+    %Identicon.Image{ hex: hex }
+  end
 
-      iex> Identicon.hello
-      :world
+  def get_color(%Identicon.Image{ hex: [r, g, b | _tail ]} = image) do
+    # %Identicon.Image{ image | color: { r, g, b } }
+    %Identicon.Image{ image | color: %{ r: r, g: g, b: b } }
+  end
 
-  """
-  def hello do
-    :world
+  def mirror_row(row) do
+    [first, second, _tail] = row
+    row ++ [second, first]
+  end
+
+  def create_grid(%Identicon.Image{ hex: hex }) do
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+    # |> mirror_row
   end
 end
